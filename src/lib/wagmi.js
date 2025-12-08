6a0b1c375e1d7accec70a6b298c0eb1bbcfc3041
Ndubuisi <ndujiovita1@gmail.com>
2025-12-08 19:41:31 +0100

    add holeskey network from wagmi chains

--- src/lib/wagmi.js
+++ src/lib/wagmi.js
@@ -1,10 +1,10 @@
 "use client";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { sepolia } from "wagmi/chains";
+import { sepolia, holesky } from "wagmi/chains";
 
 export const config = getDefaultConfig({
     appName: "NFT Marketplace",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-    chains: [sepolia],
+    chains: [sepolia, holesky],
     ssr: true,
 });
